7b99f6662557aadc44a7987f7bb699f8e16c6ad9
JaffarShah <sjeffshah@gmail.com>
2019-04-16 17:48:54 -0400

    Character counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,12 @@
 $(document).ready(function () {
-
-  console.log("Ready!");
+  // console.log("Ready!");
+  const countMax = Number($(".counter").text());
+  $(".new-tweet textarea").on("input", function(event) {
+    const display = countMax - this.textLength;
+    $(".counter", $(this).parent()).text(display);
+    if (display < 0) {
+      $(".counter", $(this).parent()).css("color","red");
+    }
+  });
+    //console.log('whatever', 
+  });
