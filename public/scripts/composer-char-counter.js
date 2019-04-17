94e2b9229ecd310b5c9a8f3753ab1cbe85ef71ec
JaffarShah <sjeffshah@gmail.com>
2019-04-17 14:15:43 -0400

    Creating the tweets

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,12 +1,15 @@
 $(document).ready(function () {
-  // console.log("Ready!");
   const countMax = Number($(".counter").text());
+
   $(".new-tweet textarea").on("input", function(event) {
     const display = countMax - this.textLength;
+    
     $(".counter", $(this).parent()).text(display);
+
     if (display < 0) {
-      $(".counter", $(this).parent()).css("color","red");
+      $(".counter", $(this).parent()).addClass('limit');
+    } else {
+      $(".counter", $(this).parent()).removeClass('limit');
     }
   });
-    //console.log('whatever', 
-  });
+});
