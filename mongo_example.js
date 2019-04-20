cb5109277cc684e871467f6adce3ae61939a747b
JaffarShah <sjeffshah@gmail.com>
2019-04-19 20:25:10 -0400

    Just gimme the array

--- mongo_example.js
+++ mongo_example.js
@@ -12,20 +12,13 @@ if (err) {
 // We have a connection to the "tweeter" db, starting here.
 console.log(`Connected to mongodb: ${MONGODB_URI}`);
 
-// ==> Let's "get all the tweets". In Mongo-speak, we "find" them.
-    db.collection("tweets").find({}, (err, results) => {
+// ==> We can just get the results as an array all at once:
+db.collection("tweets").find().toArray((err, results) => {
     if (err) throw err;
+
 
     // ==> So we Read The Fantastic Manual, right?
-
-    // ==> We can iterate on the cursor to get results, one at a time:
-    console.log("for each item yielded by the cursor:");
- // ==> We could instead just slurp the items into an array:
- results.toArray((err, resultsArray) => {
-    if (err) throw err;
-
-    console.log("results.toArray:", resultsArray);
-  });
+    console.log("results array: ", results);
   })
     // This is the end...
     db.close();
