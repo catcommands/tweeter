466c1090db89f4d7cfbbe0bde066659892db1755
JaffarShah <sjeffshah@gmail.com>
2019-04-19 20:18:26 -0400

    Working with the results

--- mongo_example.js
+++ mongo_example.js
@@ -4,26 +4,25 @@ const MongoClient = require("mongodb").MongoClient;
 const MONGODB_URI = "mongodb://localhost:27017/tweeter";
 
 MongoClient.connect(MONGODB_URI, (err, db) => {
-    if (err) {
-      console.error(`Failed to connect: ${MONGODB_URI}`);
-      throw err;
-    }
-  
-    // We have a connection to the "tweeter" db, starting here.
-    console.log(`Connected to mongodb: ${MONGODB_URI}`);
-  
-    // ==> Let's "get all the tweets". In Mongo-speak, we "find" them.
-    db.collection("tweets").find({}, (err, result) => {
-      // Lazy error handling:
-      if (err) throw err;
-  
-      // ==> Fair warning: This is going to log a lot of stuff...
-      console.log("find result: ", result);
-      console.log("type of find result: ", typeof result);
-  
-      // ==> This is inside this callback now. Think about it:
-      // This is now the "end of the program", right?.
-      db.close();
-    });
-  
+if (err) {
+    console.error(`Failed to connect: ${MONGODB_URI}`);
+    throw err;
+}
+
+// We have a connection to the "tweeter" db, starting here.
+console.log(`Connected to mongodb: ${MONGODB_URI}`);
+
+// ==> Let's "get all the tweets". In Mongo-speak, we "find" them.
+    db.collection("tweets").find({}, (err, results) => {
+    if (err) throw err;
+
+    // ==> So we Read The Fantastic Manual, right?
+
+    // ==> We can iterate on the cursor to get results, one at a time:
+    console.log("for each item yielded by the cursor:");
+    results.each((err, item) => console.log("  ", item));
+  })
+    // This is the end...
+    db.close();
   });
+  // ...
